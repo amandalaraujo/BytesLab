b8af3ccd157d957720c7317572fcc134976146e1
gestevees <estevesg988@gmail.com>
2024-04-24 21:43:39 -0300

    alteração-titulo-pagina
    alteração-titulo-pagina

--- login/login.php
+++ login/login.php
@@ -6,7 +6,7 @@
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Crude</title>
+    <title>Login | Joelma Nail Designer</title>
     <link rel="stylesheet" href="login-tela.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
